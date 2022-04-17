000d19994e6dc6da1815ebb604639846047ae819
Alexey <algoritm211@gmail.com>
2022-04-17 10:21:16 +0300

    Add new bot commands

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+  .then(() => console.log('Bot started'))
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
